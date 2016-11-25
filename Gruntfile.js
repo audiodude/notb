8baf25f02fcfca29882540270f1da37c7175dff2
Travis Briggs <audiodude@gmail.com>
2016-11-24 20:02:04 -0800

    Reorganize scss files, add Gruntfile for compiling ts and sass

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    ts: {
+      default: {
+        tsconfig: true
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'css/compiled.css': 'scss/style.scss'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('heroku:development', 'ts sass');
+  grunt.registerTask('heroku:production', 'ts sass');
+};
