ec59de55f79e62847b1c5ed0ba6e252f63163b78
Travis Briggs <audiodude@gmail.com>
2016-11-24 20:39:49 -0800

    Grunt task for installing typings

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,15 @@ module.exports = function(grunt) {
           'css/compiled.css': 'scss/style.scss'
         }
       }
+    },
+    typings: {
+      install: {}
     }
   });
   grunt.loadNpmTasks("grunt-ts");
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-typings');
 
-  grunt.registerTask('heroku:development', ['ts', 'sass']);
-  grunt.registerTask('heroku:production', ['ts', 'sass']);
+  grunt.registerTask('heroku:development', ['typings:install', 'ts', 'sass']);
+  grunt.registerTask('heroku:production', ['typings:install', 'ts', 'sass']);
 };
