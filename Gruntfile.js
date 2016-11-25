a24f417263d14f39390ac62749713784ecf0be9e
Travis Briggs <audiodude@gmail.com>
2016-11-24 20:56:25 -0800

    Fix path

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-          'css/compiled.css': 'scss/style.scss'
+          'public/css/compiled.css': 'scss/style.scss'
         }
       }
     },
